909e50e56eaacab7b81265fef809a1d68647629f
ahmed00078 <ahmedsidimohammed78@gmail.com>
2024-05-26 17:30:34 +0100

    create todo

---
+++ actions/todo.actions.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getTodoAction = async () => {
+    return await prisma.todo.findMany();
+}
+
+export const createTodoAction = async ({title, body}: {title: string; body?: string | undefined}) => {
+    return await prisma.todo.create(
+        {
+            data: {
+                title,
+                body
+            }
+        }
+    );
+}
+
+export const updateTodoAction = async () => {}
+
+export const deleteTodoAction = async () => {}
---
+++ schema/index.ts
@@ -0,0 +1,19 @@
+import { z } from "zod"
+
+export const todoFormSchema = z.object({
+    title: z
+      .string()
+      .min(4, {
+        message: "title must be at least 4 characters.",
+      })
+      .max(30, {
+        message: "title must not be longer than 30 characters.",
+      }),
+    body: z
+      .string()
+      .max(80, {
+        message: "body must not be longer than 80 characters.",
+      }).optional(),
+  })
+
+  export type TodoFormValues = z.infer<typeof todoFormSchema>
